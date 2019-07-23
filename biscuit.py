dda45bd427a9e894189fb233c0603f2bd753a27d
cssi <cssi@cssis-MacBook-Pro.local>
2019-07-23 10:57:23 -0400

    changed

--- biscuit.py
+++ biscuit.py
@@ -18,7 +18,9 @@ class loginPage(webapp2.RequestHandler):
             if existing_user:
                 pass
             else:
-
+                top_line = self.request.get("top-line")
+                bottom_line = self.request.get("bottom-line")
+                meme_url = self.request.get("template")
                 saved_dict = {
                     "name":
                     "breed":
@@ -60,108 +62,3 @@ app = webapp2.WSGIApplication([
     ('/login', loginPage ),
     ('/question', questionPage)
 ], debug=True)
-
-
-        top_line = self.request.get("top-line")
-        bottom_line = self.request.get("bottom-line")
-        meme_url = self.request.get("template")
-
-import webapp2
-import jinja2
-import os
-import time
-from models import memeInfo
-from google.appengine.ext import ndb
-from google.appengine.api import urlfetch
-import json
-
-
-
-# This initializes the jinja2 environment
-# THIS WILL BE THE SAME FOR EVERY APP YOU BUILD
-# Jinja 2.Environment is a CONSTRUCTOR
-jinja_env = jinja2.Environment(
-    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
-    extensions = ['jinja2.ext.autoescape'],
-    autoescape = True)
-
-# meme_templates = ["https://i.imgflip.com/2/30b1gx.jpg",
-# "https://i.imgflip.com/2/21tqf4.jpg", "https://i.imgflip.com/2/320xfw.jpg", "https://i.imgflip.com/2/1o00in.jpg"]
-
-# curl -d 'template_id=112126428&username=danielkelleycssi&password=cssirocks&text0=thisisthetop&text1=thisisthebottom' https://api.imgflip.com/caption_image
-
-danielkelleycssi
-cssirocks
-
-
-# "/Users/cssi-labs/python/labs/appengine"
-
-# The handler section
-class MainPage(webapp2.RequestHandler):
-    def get(self):
-
-        api_url = "https://api.imgflip.com/get_memes"
-        imgflip_response = urlfetch.fetch(api_url).content
-        imgflip_response_json = json.loads(imgflip_response)
-        print(imgflip_response_json['data']['memes'])
-        meme_templates = []
-        for meme_template in imgflip_response_json['data']['memes'][0:10]:
-            meme_templates.append(meme_template["url"])
-        meme_dict = {
-            "imgs": meme_templates
-        }
-        welcome_template = jinja_env.get_template('welcome.html')
-        self.response.write(welcome_template.render(meme_dict))
-
-
-
-class resultPage(webapp2.RequestHandler):
-    def post(self):
-
-        image_api = https://api.imgflip.com/caption_image
-
-
-        top_line = self.request.get("top-line")
-        bottom_line = self.request.get("bottom-line")
-        meme_url = self.request.get("template")
-        data_dict = {
-            "top": top_line,
-            "bottom": bottom_line,
-            "url": meme_url
-        }
-
-        memeStore = memeInfo(memeLineTop = top_line, memeImage = meme_url, memeLineBot = bottom_line)
-
-        memeStore.put()
-
-
-        result_template = jinja_env.get_template('result.html')
-        self.response.write(result_template.render(data_dict))
-
-
-
-
-
-
-
-class allmemes(webapp2.RequestHandler):
-    def get(self):
-
-
-        allMemesPlace = {
-        "memeList": memeInfo.query().fetch()
-        }
-
-
-        all_memes_template = jinja_env.get_template('allmemes.html')
-
-        self.response.write(all_memes_template.render(allMemesPlace))
-# The app configuration section
-app = webapp2.WSGIApplication(
-    [
-        ('/', MainPage),
-        ('/result', resultPage),
-        ('/all', allmemes)
-    ],
-    debug = True
-)
